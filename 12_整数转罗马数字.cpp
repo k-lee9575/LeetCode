9c9d12a071ca4b8698d37d703171aa698223e7a2
李超 <K.lee.9575@outlook.com>
2019-08-02 23:40:39 +0800

    12_整数转罗马数字

---
+++ 12_整数转罗马数字.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+class Solution {
+public:
+	string intToRoman(int num) {
+		string ans;
+		int nData[5] = { 10000, 1000, 100, 10, 1 };
+		char cBase[5] = { ' ', 'M', 'C', 'X', 'I' };
+		char cMid[5] = { ' ', ' ', 'D', 'L', 'V' };
+		int nCount;
+		for (int i = 1; i <= 4; i++) {
+			nCount = num / nData[i];
+			num = num % nData[i];
+
+			switch (nCount)
+			{
+			case 4:
+				ans += cBase[i];
+				ans += cMid[i];
+				break;
+			case 9:
+				ans += cBase[i];
+				ans += cBase[i - 1];
+				break;
+			default:
+				if (nCount >= 5) {
+					ans += cMid[i];
+					nCount -= 5;
+				}
+				for (int j = 0; j < nCount; j++) {
+					ans += cBase[i];
+				}
+				break;
+			}
+		}
+		return ans;
+
+	}
+};
--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10,8 +10,8 @@
 //#include "8_字符串转换整数.cpp"
 //#include "9_回文数.cpp"
 //#include "10_正则表达式匹配.cpp"
-#include "11_盛最多水的容器.cpp"
-
+//#include "11_盛最多水的容器.cpp"
+#include "12_整数转罗马数字.cpp"
 
 int main()
 {
@@ -42,11 +42,16 @@ int main()
 	//cout << ans.isMatch("bcaab", "bc.*ab") << endl;
 
 	//11_盛最多水的容器
+	//Solution ans;
+	//vector<int> height;
+	//height.push_back(2);
+	//height.push_back(1);
+	//cout << ans.maxArea(height) << endl;
+
+	//12_整数转罗马数字
 	Solution ans;
-	vector<int> height;
-	height.push_back(2);
-	height.push_back(1);
-	cout << ans.maxArea(height) << endl;
+	cout << ans.intToRoman(1024) << endl;
+	cout << ans.intToRoman(1994) << endl;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
